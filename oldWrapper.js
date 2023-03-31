479116880c227481197281d33d74227dcba60cec
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2023-03-31 15:16:28 -0700

    Update oldWrapper.js

--- oldWrapper.js
+++ oldWrapper.js
@@ -18,11 +18,67 @@ const _getAndSet = (id) => {
 		el.setAttribute(_dataAttributeType, e[1]["value"]); // default
 	});
 };
+
+//Ajax Functions
+function charge(data) {
+	$(document).ready(function () {
+		$("#btnOkSourcecharge").click(function () {
+			$.ajax({
+				url: "/api/transactions/Sourcecharge",
+				beforeSend: function () {
+					$("#btnCancelSourcecharge").attr("disabled", true);
+					$("#btnOkSourcecharge").attr("disabled", true);
+				},
+				contentType: "application/json",
+				data: JSON.stringify(data),
+				type: "POST",
+				success: function (data) { },
+				error: function (error) { },
+			});
+		});
+	});
+}
+
+function refund(data) {
+	$(document).ready(function () {
+		$("#btnOkRefund").click(function () {
+			$.ajax({
+				url: "/api/transactions/refund",
+				beforeSend: function () {
+					$("#btnCancelRefund").attr("disabled", true);
+					$("#btnOkRefund").attr("disabled", true);
+				},
+				contentType: "application/json",
+				data: JSON.stringify(data),
+				type: "POST",
+				success: function (data) {
+					if (!data.error) {
+						$("#txtRefundSuccess").text("Refund processed");
+						$("#txtRefundFailure").text("");
+					} else {
+						$("#txtRefundSuccess").text("");
+						$("#txtRefundFailure").text("Payment Id not found");
+					}
+					$("#btnCancelRefund").removeAttr("disabled");
+					$("#btnOkRefund").removeAttr("disabled");
+					$("#modalRefundAmount").modal("hide");
+					$("#modalRefundResult").modal("show");
+				},
+				error: function (error) {
+					$("#btnCancelRefund").removeAttr("disabled");
+					$("#btnOkRefund").removeAttr("disabled");
+					$("#txtRefundSuccess").text("");
+					$("#txtRefundFailure").text(error.responseJSON.message);
+					$("#modalRefundResult").modal("show");
+				},
+			});
+		});
+	});
+}
+
 // Class that wraps Accept.blue iFrame
 class HostedIFrame {
 	cardForm;
-	#scriptMount = "_iFrame"; //private field
-	#observeParent = document.querySelector("head");
 	constructor(tokenSourceKey, iframeMount, btnMount) {
 		this.tokenSourceKey = tokenSourceKey;
 		this.iframeMount = `#${iframeMount}`;
@@ -30,15 +86,12 @@ class HostedIFrame {
 	}
 
 	init() {
-		this._observe(() => {
-			const tokenizationSourceKey = this.tokenSourceKey;
-			const hostedTokenization = new window.HostedTokenization(
-				tokenizationSourceKey
-			);
-			this.cardForm = hostedTokenization
+		this._onLoad(() => {
+			this.cardForm = new window.HostedTokenization(this.tokenSourceKey)
 				.create("card-form")
 				.mount(this.iframeMount);
 		});
+
 		return this;
 	}
 	//Submits and mounts the result of the card form. Returns the nonce token, expirymonth, expiryyear, and cardtype and last4
@@ -47,9 +100,15 @@ class HostedIFrame {
 		this._clicked(this.btnMount, () => {
 			const resultMount = this._resultMount; //set outside the promise to access HostedIFrame
 			const errorResult = this._errorMount; //set outside the promise to access HostedIFrame
+			const sourceVerification = this._sourceVerification;
 			this.cardForm
 				.getNonceToken()
 				.then((result) => {
+					sourceVerification({
+						Source: 'nonce-' + result.nonce,
+						Expiry_Month: result.expiryMonth,
+						Expiry_Year: result.expiryYear,
+					});
 					resultMount(
 						result,
 						submitMounts.form,
@@ -66,12 +125,12 @@ class HostedIFrame {
 					throw new Error(error);
 				});
 		});
+		return this;
 	}
 	//used to preset styles based on an object that will be provided with multiple items within that will change the style
 	styles(styles) {
-		this._observe(() => {
-			this.cardForm.setStyles(styles);
-		});
+		this._onLoad(() => this.cardForm.setStyles(styles));
+		return this;
 	}
 
 	_errorMount(errorMount, _mainError, textContent = false) {
@@ -93,34 +152,34 @@ class HostedIFrame {
 			[expiryYearMount]: { value: result.expiryYear },
 			[cardTypeMount]: { value: result.cardType },
 			[last4Mount]: { value: result.last4 },
-			// [formMount]: { submit: true },
+			[formMount]: { submit: true },
 		});
 	}
 	//takes an arrow function as a parameter
-	_observe(injectedCode) {
-		//First checks to see if the script tag has been added before running code
-		const mutationObserver = new MutationObserver((entries) => {
-			entries.forEach((e) => {
-				if ((e.addedNodes[0].id = this.#scriptMount)) {
-					injectedCode(); //any code you write in function will be here
-				}
-			});
-		});
-		mutationObserver.observe(this.#observeParent, { childList: true });
+	_onLoad(injectedCode) {
+		window.addEventListener("load", injectedCode);
 	}
 	//event listener to see if element based on Id was clicked or not
 	_clicked(id, injectedCode) {
 		document.getElementById(id).addEventListener("click", injectedCode);
 	}
-
-	_VerifyAndSave() {
-		//code
-	}
-
-	Charge() {
-		//code
-	}
-	_ajaxCall() {
-		//code
+	//Ajax function that does a source verification and returns a token to later be stored in the customers profile
+	_sourceVerification(dataObj) {
+		$(document).ready(function () {
+			$.ajax({
+				url: "/api/transactions/sourceverification",
+				contentType: "application/json",
+				data: JSON.stringify(dataObj),
+				type: "POST",
+				success: function (data) {
+					console.log("Success:");
+					console.log(data);
+				},
+				error: function (error) {
+					console.log("Error:");
+					console.log(error);
+				},
+			});
+		});
 	}
 }
