c4ebbd742aa790c7b6a4619ba7e84ba6241d0770
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2023-03-31 18:56:40 -0700

    Update SourceVerificationRequest.cs

--- SourceVerificationRequest.cs
+++ SourceVerificationRequest.cs
@@ -24,39 +24,4 @@ namespace SO_Virtual_Terminal.Models.AcceptBlue
 		[JsonPropertyName("expiry_year")]
 		public int Expiry_Year { get; set; }
 	}
-
-	public class Transaction_Details
-	{
-		[JsonPropertyName("description")]
-		public string Description { get; set; }
-		[JsonPropertyName("clerk")]
-		public string Clerk { get; set; }
-		[JsonPropertyName("terminal")]
-		public string Terminal { get; set; }
-		[JsonPropertyName("client_ip")]
-		public string Client_IP { get; set; }
-		[JsonPropertyName("signature")]
-		public string Signature { get; set; }
-	}
-
-	public class Transaction_Flags
-	{
-		[JsonPropertyName("allow_partial_approval")]
-		public bool Allow_Partial_Approval { get; set; }
-		[JsonPropertyName("is_recurring")]
-		public bool Is_Recurring { get; set; }
-		[JsonPropertyName("is_installment")]
-		public bool Is_Installment { get; set; }
-		[JsonPropertyName("is_customer_initiated")]
-		public bool Is_Customer_Initiated { get; set; }
-		[JsonPropertyName("cardholder_present")]
-		public bool Cardholder_Present { get; set; }
-		[JsonPropertyName("card_present")]
-		public bool Card_Present { get; set; }
-	}
-
-	public class Terminal
-	{
-	}
-
 }
