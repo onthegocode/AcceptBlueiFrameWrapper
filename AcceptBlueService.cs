f0ec62bf674b803f111a909bd78c1276c51a43fb
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2023-03-31 18:59:49 -0700

    Update AcceptBlueService.cs

--- AcceptBlueService.cs
+++ AcceptBlueService.cs
@@ -1,6 +1,6 @@
 //Used to Verify and save nonce token by returning a longer term token
-		public SourceVerificationResponse SourceVerification(SourceVerificationRequest verifyAndSaveRequest)
-		{
+	public SourceVerificationResponse SourceVerification(SourceVerificationRequest verifyAndSaveRequest)
+	{
             string txtContent = JsonSerializer.Serialize(verifyAndSaveRequest, options);
             var endpoint = Config.AcceptBlue.Paths.Verify;
             var payload = new StringContent(txtContent, Encoding.UTF8, "application/json");
@@ -15,3 +15,21 @@
             }
             return sourceVerificationResponse;
         }
+
+	//Used to Charge card based on saved Token
+	public SourceChargeResponse SourceCharge(SourceChargeRequest sourceChargeRequest)
+	{
+		string txtContent = JsonSerializer.Serialize(sourceChargeRequest, options);
+		var endpoint = Config.AcceptBlue.Paths.Charge;
+		var payload = new StringContent(txtContent, Encoding.UTF8, "application/json");
+		SourceChargeResponse sourceChargeResponse = PostAsync<SourceChargeResponse>(endpoint, payload);
+		if (!string.IsNullOrWhiteSpace(sourceChargeResponse.Error_Code))
+		{
+			throw new Exception(sourceChargeResponse.Error_Message);
+		}
+		if (sourceChargeResponse.Status_Code != "A")
+		{
+			throw new Exception(sourceChargeResponse.Status);
+		}
+		return sourceChargeResponse;
+	}
