234814f015f64f197fb3fb49fdacecaa0befba72
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2023-04-05 14:16:30 -0700

    Update wrapper.js

--- wrapper.js
+++ wrapper.js
@@ -9,19 +9,10 @@
 
 const _dataAttributeType = "value"; //Used to easily set the data attribute. Using convention _ to indicate its a variable not to be used
 
-//Used to Get an element based on id and Set a method to it ex: setAttribute, submit, and textContent - setAttribute is always set
-const _getAndSet = (id) => {
-	Object.entries(id).forEach((e) => {
-		const el = document.getElementById(e[0]);
-		if (e[1]["submit"] || e[1]["text"]) {
-			e[1]["submit"] ? el.submit() : (el.textContent = e[1]["value"]);
-		}
-		el.setAttribute(_dataAttributeType, e[1]["value"]); // default
-	});
-};
 
 //Used to call the SourceCharge Method inside the TransactionController. This function would likely have to be wrapped in a click event to know when to go off. Takes a token as an argument
 function charge(object) {
+	//returns a promise to allow for the user to capture the Json response
 	return new Promise(function (resolve, reject) {
 		$(document).ready(function () {
 			$.ajax({
@@ -30,10 +21,10 @@ function charge(object) {
 				data: JSON.stringify(object),
 				type: "POST",
 				success: function (data) {
-					resolve(data);
+					resolve(data); // returns Json response
 				},
 				error: function (error) {
-					reject(error);
+					reject(error); // returns error response returned from the AcceptBlueService
 				},
 			});
 		});
@@ -80,11 +71,14 @@ function refund(data) {
 
 // Class that wraps Accept.blue iFrame
 class HostedIFrame {
-	cardForm;
+	cardForm; //used to store the form
+
 	constructor(tokenSourceKey, iframeMount, btnMount) {
 		this.tokenSourceKey = tokenSourceKey;
 		this.iframeMount = `#${iframeMount}`;
 		this.btnMount = `${btnMount}`;
+
+		//Auto Initiate
 		this._onLoad(() => {
 			this.cardForm = new window.HostedTokenization(this.tokenSourceKey)
 				.create("card-form")
@@ -92,33 +86,24 @@ class HostedIFrame {
 		});
 	}
 
-	/*init() {
-		
-
-		return this;
-	}*/
-	//Submits and mounts the result of the card form. Returns the nonce token, expirymonth, expiryyear, and cardtype and last4
+	//Submits and mounts the result of the card form. Returns a Json response that contains the nonce token, expirymonth, expiryyear, and cardtype and last4
 	//Takes an object as the argument
-	submit(submitMounts) {
-		var main = this;
+	submit(data = {}) {
+		var self = this;
 		return new Promise(function (resolve, reject) {
-			main._clicked(main.btnMount, () => {
-				/*// const resultMount = this._resultMount; //set outside the promise to access HostedIFrame
-				const errorResult = main._errorMount; //set outside the promise to access HostedIFrame
-				const sourceVerification = main._sourceVerification; //set outside the promise to access HostedIFrame*/
-				main.cardForm
+			self._clicked(self.btnMount, () => {
+				self.cardForm
 					.getNonceToken()
 					.then((result) => {
 						//calls the api to verify nonce token and returns token
 						const newObj = {
-							Name: submitMounts.Name ? submitMounts.Name : "",
-							Avs_Address: submitMounts.Avs_Address ? submitMounts.Avs_Address : "",
-							Avs_Zip: submitMounts.Avs_Zip ? submitMounts.Avs_Zip : "",
-							Software: submitMounts.Software ? submitMounts.Software : "Hi :D",
+							Name: data.Name,
+							Avs_Address: data.Avs_Address,
+							Avs_Zip: data.Avs_Zip,
+							Software: data.Software,
 							Source: "nonce-" + result.nonce,
 							Expiry_Month: result.expiryMonth,
 							Expiry_Year: result.expiryYear,
-
 						}
 
 						$.ajax({
@@ -133,11 +118,9 @@ class HostedIFrame {
 								reject(error); //returns the error
 							},
 						});
-
-						/*_getAndSet({ [submitMounts.form]: { submit: true } });*/ //submits the form
 					})
 					.catch((mainError) => {
-						reject(mainError); //returns the error 
+						reject(mainError); //returns the error
 					});
 			});
 		});
